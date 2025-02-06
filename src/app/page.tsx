851f0959d312ffa30dc995874c280f75e9a539a4
alexis-nauleau <a.nauleau@hotmail.fr>
2025-02-06 22:14:19 +0100

    corpov2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,17 @@ const page = () => {
   return (
     <div>
       <ul>meilleurs pnj
-        <li>Bruno ironBright    légendaire</li>
-        <li>Chef michel   epic</li>
-        <li>Dario   epiqyue</li>
-        <li>Franke maitre artisan   epic</li>
+        <li>Bruno Ironbright, Engineering Giant     légendaire  Targets: Bicycle Factory, Sewing Machine Factory, Cab Assembly Line, Motor Assembly Plant, Fan Factory, Scooter Factory, Sewing Machine Factory</li>
+        <li>Chef michel   epic  Cannery</li>
+        <li>Dario   epiqyue    Bicycle Factory, Sewing Machine Factory</li>
+        <li>Franke maitre artisan   epic  Fur Dealer</li>
         <li>Feras   epique</li>
         <li>frere hillarius   légendaire  rhum</li>
         <li>sir lewis brindley  epic  rhum</li>
-        <li>Gerhard fuch  légendaire</li>
-        <li>Polenisation manuelle   Légendaire   Nouveau monde</li>
-        <li>scécateur   épic</li>
-        <li>Prof ram devi   Légendaire</li>
-        <li>Salvador  epic  Nouveau monde</li>
-        <li>Steven MacLeod  Légendaire</li>
+        <li>Gerhard fuch  légendaire  Spectacle Factory</li>
+        <li>Prof ram devi   Légendaire Light Bulb Factory, Gramophone Factory, Chemical Plant: Film Reels, Fan Factory</li>
+        <li>Salvador  epic   Cigar Factory, Chocolate Factory, Coffee Roaster, All New World Chemical Plants Nouveau monde</li>
+        <li>Steven MacLeod  Légendaire   All Clay Pits, All Saltpetre Works, Sand Mine, Limestone Quarry, Clay Collector, Bauxite Mine</li>
 
       </ul>
     </div>
